23c23ed8cff86516c27a559ff5cd09fda9d62b87
naumanmehdi <QWtj4etSRNPW2f3>
2022-04-20 03:00:51 +0500

    smart contract changes

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("Yo yo, I am SMTC and I am a smart contract");
+    }
+
+    function wave() public {
+      totalWaves += 1;
+      console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns(uint256) {
+      console.log("we have %d total waves!", totalWaves, "waved by", msg.sender);
+      return totalWaves;
     }
 }
